2a17e24534cbf805bc9e727b0c14a4f0597daca5
Dylan Warren <dylanwarren19@gmail.com>
2022-02-06 03:41:11 -0600

    Basic auth/data request

--- RoleBot.API/Middleware/IDiscordApi.cs
+++ RoleBot.API/Middleware/IDiscordApi.cs
@@ -1,9 +1,16 @@
 using Refit;
+using RoleBot.API.Models;
+
 namespace RoleBot.API.Middleware;
 
-[Headers("Authorization: Bearer")]
 public interface IDiscordApi
 {
-    [Post("/token")]
-    Task<string> GetAccessToken([Body(BodySerializationMethod.UrlEncoded)] Dictionary<string, object> data);
+    [Post("/oauth2/token")]
+    Task<string> GetAccessToken([Body(BodySerializationMethod.UrlEncoded)] AuthData data);
+
+    [Post("/oauth2/token")]
+    Task<string> RefreshToken([Body(BodySerializationMethod.UrlEncoded)] AuthData data);
+
+    [Get("/users/@me")]
+    Task<string> GetUser([Authorize] string authorization);
 }
