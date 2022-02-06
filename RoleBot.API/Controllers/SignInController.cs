0da3823d44531dcbea8e2195b35d0571481757d5
Dylan Warren <dylanwarren19@gmail.com>
2022-02-06 16:56:30 -0600

    Still not done but a better implementation of signin

--- RoleBot.API/Controllers/SignInController.cs
+++ RoleBot.API/Controllers/SignInController.cs
@@ -1,8 +1,10 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RoleBot.API.Middleware;
 using RoleBot.API.Models;
+using RoleBot.Infrastructure.Services.Interfaces;
 
 namespace RoleBot.API.Controllers;
 
@@ -11,29 +13,58 @@ namespace RoleBot.API.Controllers;
 public class SignInController : ControllerBase
 {
     private readonly IDiscordApi _api;
-    private AuthData _authData;
-    public SignInController(IDiscordApi api, AuthData authData)
+    private readonly IJwtService _jwtService;
+    private readonly AuthData _authData;
+    public SignInController(IDiscordApi api, AuthData authData, IJwtService jwtService)
     {
         _api = api;
         _authData = authData;
+        _jwtService = jwtService;
     }
     
     [HttpPost]
     public async Task<IActionResult> Auth(string code)
     {
-        var userData = _authData with { Code = code };
-
-        var jsonString = await _api.GetAccessToken(userData);
-
-        var json = JsonObject.Parse(jsonString);
-
-        if (json != null && json["access_token"] != null)
+        try
         {
+            var userData = _authData with { Code = code };
+
+            var jsonString = await _api.GetAccessToken(userData);
+
+            var json = JsonObject.Parse(jsonString);
+
+            if (json?["access_token"] == null)
+            {
+                return Unauthorized();
+            }
+
             var accessToken = json["access_token"].ToString();
 
-            return Ok(await _api.GetUser(accessToken));
+            var userJsonString = await _api.GetUser(accessToken);
+            var userGuildJsonString = await _api.GetUserGuilds(accessToken);
+
+            var token = _jwtService.GenerateAuthToken(accessToken, userJsonString, userGuildJsonString);
+
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            
+            _jwtService.AppendTokenCookie(Response.Cookies, token);
+            
+            var user = JsonConvert.DeserializeObject(userJsonString);
+            var guilds = JsonConvert.DeserializeObject(userGuildJsonString);
+
+            var serializedJson = JsonConvert.SerializeObject(new
+            {
+                user, guilds
+            });
+
+            return Ok(JsonObject.Parse(serializedJson));
         }
-        
-        return Ok(json);
+        catch
+        {
+            return Unauthorized();
+        }
     }
 }
